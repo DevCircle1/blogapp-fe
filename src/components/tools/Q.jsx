6cf623048e7ee6091ba70de19853b9b96cd42d38
Abdullahbutt-143 <asohailbutt34@gmail.com>
2025-10-12 17:45:38 +0500

    a

--- src/components/tools/Q.jsx
+++ src/components/tools/Q.jsx
@@ -167,7 +167,6 @@ const Q = () => {
                     </span>
                   </span>
                 </div>
-                {/* Right side: Author */}
                 <div className="text-gray-600 sm:text-right text-sm sm:mt-0 mt-1">
                   <span className="font-medium text-indigo-600">
                     {question.user_email || "Anonymous"}
